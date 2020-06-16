56a4f4eb1df03a028c705e66bdc08d56b2295f05
[Meenakshi Bharadwaj] <[minakshi29bharadwaj@gmail.com]>
2020-06-16 21:59:51 +0530

    Re-coded Function copyTextFunc

--- Copy to Clipboard Button Using JS/clipboardcpy.js
+++ Copy to Clipboard Button Using JS/clipboardcpy.js
@@ -1,5 +1,14 @@
-Function copyText(){
-    let textarea = document.querySelector("#textArea");
-    textarea.select();
+function copyTextFunc() {
+    /* Get the text field */
+    var copyText = document.getElementById("textArea");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+  
+    /* Copy the text inside the text field */
     document.execCommand("copy");
-}
+  
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+  }
